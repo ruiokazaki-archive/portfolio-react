a78ecd3ef85c0559f29bf55e09a719dbb57b11c3
RuiOkazaki <okazaki200226@gmail.com>
2021-10-04 11:07:14 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import { VFC, useEffect } from 'react';
+import { Navigate, Route, Routes, useLocation } from 'react-router';
+
+import Home from 'components/pages/Home';
+
+import './App.css';
+
+const App: VFC = () => {
+  const { hash, pathname } = useLocation();
+
+  useEffect(() => {
+    if (!hash) window.scrollTo(0, 0);
+  }, [hash, pathname]);
+
+  return (
+    <div className="container">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* <Route path="works" element={<Works />}>
+          <Route path=":workId" element={<Work />} />
+        </Route> */}
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/pages/Home.tsx
@@ -0,0 +1,9 @@
+import { VFC } from 'react';
+
+const Home: VFC = () => (
+  <div>
+    <h1>test</h1>
+  </div>
+);
+
+export default Home;
