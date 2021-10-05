d2cd54b8eab5679bb9f70883b6fb1b19a4fe4d3e
RuiOkazaki <okazaki200226@gmail.com>
2021-10-05 22:29:45 +0900

    works create

---
+++ src/data/works.ts
@@ -0,0 +1,10 @@
+export type Work = {
+  id: number;
+  name: string;
+  concept: string;
+  motivation: string;
+  production: string;
+  position: string;
+  useTime: string;
+  Tools: string;
+};
