5ec7373cc561635808f39f4fe49e58d0d6d630ef
RuiOkazaki <okazaki200226@gmail.com>
2021-10-07 20:47:27 +0900

    refactor: add templates directory & move SectionWrap components

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,5 +1,5 @@
 import { VFC } from 'react';
-import SectionWrap from './SectionWrap';
+import SectionWrap from '../templates/SectionWrap';
 import SectionBackground from '../../containers/organisms/SectionBackground';
 
 const Home: VFC = () => (
