ebb38d169e48c1c479ec2b153ee2470bcba7e282
RuiOkazaki <okazaki200226@gmail.com>
2021-10-08 23:55:31 +0900

    add: create SectionTitle components

---
+++ src/components/atoms/SectionTitle.style.tsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+import tw from 'tailwind-styled-components';
+
+const SectionTitleStyle = styled.h2`
+  font-size: 18px;
+  color: #f858f1;
+`;
+const SectionTitle = tw(SectionTitleStyle)`
+  font-normal relative mb-8
+`;
+
+export default SectionTitle;
---
+++ src/components/atoms/SectionTitle.tsx
@@ -0,0 +1,10 @@
+import { VFC } from 'react';
+
+type Props = {
+  sectionTitle: string;
+};
+
+const SectionBackground: VFC<Props> = ({ sectionTitle }) => (
+);
+
+export default SectionBackground;
