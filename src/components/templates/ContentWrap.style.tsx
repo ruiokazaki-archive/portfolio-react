f4c495c523348439c223d6c80588e12a25d7a035
RuiOkazaki <okazaki200226@gmail.com>
2021-10-09 10:57:05 +0900

    update: Section の border を作成

--- src/components/templates/ContentWrap.style.tsx
+++ src/components/templates/ContentWrap.style.tsx
@@ -4,6 +4,17 @@ import tw from 'tailwind-styled-components';
 const ContentWrapStyle = styled.div`
   width: 868px;
   padding: 75px 22px;
+  &:before {
+    content: '';
+    display: block;
+    width: 2px;
+    position: absolute;
+    background-color: #f858f1;
+    top: 0;
+    left: -2px;
+    z-index: 2;
+    height: 100%;
+  }
 `;
-const ContentWrap = tw(ContentWrapStyle)`mx-auto`;
+const ContentWrap = tw(ContentWrapStyle)`mx-auto relative`;
 export default ContentWrap;
