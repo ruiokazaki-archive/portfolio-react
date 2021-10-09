6167557709b4662bb6ab7e0ccf319a7f0c6d8b2a
RuiOkazaki <okazaki200226@gmail.com>
2021-10-09 15:41:17 +0900

    update: sectionwrap に background, title 等を まとめた

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,16 +1,9 @@
 import { VFC } from 'react';
 import SectionWrap from '../templates/SectionWrap';
-import ContentWrap from '../templates/ContentWrap';
-import SectionBackground from '../../containers/molecules/SectionBackground';
-import SectionTitle from '../atoms/SectionTitle';
 
 const Home: VFC = () => (
-  <SectionWrap>
-    <SectionBackground sectionTitle="MainContents" />
-    <ContentWrap>
-      <SectionTitle sectionTitle="MainContents" />
-      <p>てすとてきすと</p>
-    </ContentWrap>
+  <SectionWrap sectionTitle="MainContent">
+    <p>てすとてきすと</p>
   </SectionWrap>
 );
 
--- src/components/templates/SectionWrap.tsx
+++ src/components/templates/SectionWrap.tsx
@@ -1,12 +1,22 @@
 import { VFC } from 'react';
 import SectionWrap from './SectionWrap.style';
+import ContentWrap from './ContentWrap';
+import SectionBackground from '../../containers/molecules/SectionBackground';
+import SectionTitle from '../atoms/SectionTitle';
 
 type Props = {
+  sectionTitle: string;
   children: React.ReactNode;
 };
 
-const SectionBackground: VFC<Props> = ({ children }) => (
-  <SectionWrap>{children}</SectionWrap>
+const Section: VFC<Props> = ({ sectionTitle, children }) => (
+  <SectionWrap>
+    <SectionBackground sectionTitle={sectionTitle} />
+    <ContentWrap>
+      <SectionTitle sectionTitle={sectionTitle} />
+      {children}
+    </ContentWrap>
+  </SectionWrap>
 );
 
-export default SectionBackground;
+export default Section;
