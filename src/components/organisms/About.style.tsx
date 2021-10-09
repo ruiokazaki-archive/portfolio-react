0d21ef79e19d180b9ef33ee4a423eef09bf0c336
RuiOkazaki <okazaki200226@gmail.com>
2021-10-09 17:14:32 +0900

    update: About components complete

---
+++ src/components/organisms/About.style.tsx
@@ -0,0 +1,61 @@
+import styled from 'styled-components';
+import tw from 'tailwind-styled-components';
+
+// Contents Wrap
+
+// Left contnests
+const LeftContentsStyle = styled.section`
+  width: 487px;
+  height: 400px;
+`;
+export const LeftContents = tw(LeftContentsStyle)`flex flex-col`;
+
+const ProfileNameStyle = styled.h3`
+  line-height: 49px;
+  letter-spacing: 10px;
+  font-size: 42px;
+  margin-top: 10px;
+  margin-bottom: 10px;
+`;
+export const ProfileName = tw(ProfileNameStyle)`font-bold order-2`;
+const ProfileSchoolStyle = styled.p`
+  font-size: 14px;
+`;
+export const ProfileSchool = tw(ProfileSchoolStyle)` order-1`;
+
+const ProfileIntroductionStyle = styled.p`
+  font-size: 14px;
+  line-height: 28px;
+`;
+export const ProfileIntroduction = tw(
+  ProfileIntroductionStyle,
+)`text-justify order-3`;
+
+const ProfileImageStyle = styled.figure`
+  img {
+    width: 100%;
+    border-radius: 36px;
+  }
+`;
+export const ProfileImage = tw(ProfileImageStyle)`w-full shadow`;
+
+// Right contnests
+export const RightContents = styled.div`
+  width: 300px;
+`;
+
+const LinkWrapStyle = styled.div`
+  margin: 10px 15px 0 15px;
+`;
+export const LinkWrap = tw(LinkWrapStyle)`flex justify-between`;
+
+const LinkStyle = styled.a`
+  font-size: 18px;
+  line-height: 21px;
+`;
+export const Link = tw(LinkStyle)`flex items-center text-pink1 shadow`;
+
+const LinkIconStyle = styled.figure`
+  margin-right: 5px;
+`;
+export const LinkIcon = tw(LinkIconStyle)`flex items-center justify-center`;
--- src/components/organisms/About.tsx
+++ src/components/organisms/About.tsx
@@ -1,9 +1,66 @@
 import { VFC } from 'react';
 import SectionWrap from '../templates/SectionWrap';
+import {
+  LeftContents,
+  RightContents,
+  ProfileName,
+  ProfileSchool,
+  ProfileIntroduction,
+  ProfileImage,
+  LinkWrap,
+  Link,
+  LinkIcon,
+} from './About.style';
+
+// images
+import myself from '../../assets/images/myself.jpg';
+import github from '../../assets/svg/github.svg';
+import instagram from '../../assets/svg/instagram.svg';
 
 const About: VFC = () => (
   <SectionWrap sectionTitle="About">
-    <p>てすとてきすと</p>
+    <div className="flex items-center justify-between">
+      <LeftContents>
+        <ProfileName>RUI OKAZAKI</ProfileName>
+        <ProfileSchool>
+          ECCコンピュータ専門学校 WEBデザイナー専攻2年
+        </ProfileSchool>
+        <ProfileIntroduction>
+          私は人を笑顔にする事と教える事が好きです。
+          <br />
+          専門学校に入り友人にプログラミングを教えたことがあります。
+          <br />
+          プログラムが嫌いな友人が自分自身の力で出来るようになるたびに笑顔になってくれました。
+          <br />
+          私も周りの人もそんな友人を見て、笑顔になり幸せな気持ちになったのでプログラミングにはそれだけの価値や力があるのだと確信しました。
+          <br />
+          私は将来好きなプログラミングに関わりながらも、周りが笑顔になるように働きたいと考えています。
+          <br />
+          「一緒に働く人はもちろん、作成したものを使ってくれる人まで良い気分で居ることが出来る」
+          <br />
+          そんな人の気持ちを汲み取れるエンジニアを目指しています！！
+        </ProfileIntroduction>
+      </LeftContents>
+      <RightContents>
+        <ProfileImage>
+          <img src={myself} alt="岡崎流依の写真" />
+        </ProfileImage>
+        <LinkWrap>
+          <Link href="https://github.com/OkazakiRui" target="ruru">
+            <LinkIcon>
+              <img src={github} alt="github" />
+            </LinkIcon>
+            OkazakiRui
+          </Link>
+          <Link href="https://www.instagram.com/asahanemutaiyo/" target="ruru">
+            <LinkIcon>
+              <img src={instagram} alt="instagram" />
+            </LinkIcon>
+            asahanemutaiyo
+          </Link>
+        </LinkWrap>
+      </RightContents>
+    </div>
   </SectionWrap>
 );
 
