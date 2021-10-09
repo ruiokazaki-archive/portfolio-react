bf7b84cbb500df9d7c81c84ed81744cedc833538
RuiOkazaki <okazaki200226@gmail.com>
2021-10-09 20:52:29 +0900

    update: age を自動計算するようにした

--- src/data/ProfileSkillCode.ts
+++ src/data/ProfileSkillCode.ts
@@ -17,15 +17,30 @@ type constructor = {
   email: `${string}@${string}.${string}`;
 };
 
+const birthday = {
+  year: 2002,
+  month: 2,
+  date: 6,
+};
+
+const getAge = () => {
+  const now = new Date();
+  const thisYear = new Date(
+    now.getFullYear(),
+    birthday.month - 1,
+    birthday.date,
+  );
+  let age: number = now.getFullYear() - birthday.year;
+  if (now < thisYear) age -= 1;
+
+  return age;
+};
+
 export const ProfileData: ProfileCode = {
   constructor: {
     name: 'string',
-    age: 1,
-    birthday: {
-      year: 2002,
-      month: 2,
-      date: 6,
-    },
+    age: getAge(),
+    birthday,
     live: 'Kobe',
     email: 'okazaki200226@gmail.com',
   },
