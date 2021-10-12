bd9c384aef93552abca8e1cd347b26ac56e4b468
RuiOkazaki <okazaki200226@gmail.com>
2021-10-12 11:48:00 +0900

    update: ProfileElementCode を修正した

--- src/containers/molecules/ProfileElementCode.tsx
+++ src/containers/molecules/ProfileElementCode.tsx
@@ -17,29 +17,29 @@ const ProfileElementCode: VFC<Props> = ({ profileData }) => {
     return char.slice(0, -2);
   };
 
-  return (
-    <>
-      <p>class OkazakiRui &#123;;</p>
-      <p>　constructor( ) &#123;;</p>
-      <p>　　this.name = &#34;{profileData.constructor.name}&#34;;</p>
-      <p>　　this.age = {profileData.constructor.age};</p>
-      <p>
-        　　this.birthday = &#34;
-        {`${profileData.constructor.birthday.year}年${profileData.constructor.birthday.month}月${profileData.constructor.birthday.date}日`}
-        &#34;;
-      </p>
-      <p>　　this.live = &#34;{profileData.constructor.live}&#34;;</p>
-      <p>　　this.email = &#34;{profileData.constructor.email}&#34;;</p>
-      <p>　&#125;</p>
-      <p>　programmingLanguage( ) &#123;</p>
-      <p>　　return [ {createChar(profileData.programmingLanguage)} ];</p>
-      <p>　&#125;</p> <p>　mainLanguage( ) &#123;</p>
-      <p>　　return [ {createChar(profileData.mainLanguage)} ];</p>
-      <p>　&#125;</p> <p>　tools( ) &#123;</p>
-      <p>　　return [ {createChar(profileData.tools)} ];</p> <p>　&#125;</p>
-      <p>&#125;</p>
-    </>
-  );
+  const beforeCode = `class RuiOkazaki {
+    constructor() {
+      this.name = "${profileData.constructor.name}";
+      this.age = ${profileData.constructor.age};
+      this.birthday = "${profileData.constructor.birthday.year}年${
+    profileData.constructor.birthday.month
+  }月${profileData.constructor.birthday.date}日";
+      this.live = "${profileData.constructor.live}";
+      this.email = "${profileData.constructor.email}";
+    }
+    programmingLanguage() {
+      return [ ${createChar(profileData.programmingLanguage)} ];
+    }
+    mainLanguage() {
+      return [ ${createChar(profileData.mainLanguage)} ];
+    }
+    tools() {
+      return [ ${createChar(profileData.tools)} ];
+    }
+  }
+  `;
+
+  return <div dangerouslySetInnerHTML={{ __html: sanitizer(beforeCode) }} />;
 };
 
 export default ProfileElementCode;
