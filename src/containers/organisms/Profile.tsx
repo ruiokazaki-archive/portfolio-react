69a9f53750f86d7541934afe8c05fbba3da7827c
RuiOkazaki <okazaki200226@gmail.com>
2021-10-14 13:39:48 +0900

    add: create Profile components

---
+++ src/containers/organisms/Profile.tsx
@@ -0,0 +1,9 @@
+import { VFC } from 'react';
+
+import Profile from '../../components/organisms/Profile';
+import { ProfileData } from '../../data/ProfileSkillCode';
+// import { ProfileData } from '@/data/ProfileSkillCode';
+
+const EnhancedProfile: VFC = () => <Profile profileData={ProfileData} />;
+
+export default EnhancedProfile;
