10a309d44de1e646f3240721b9c68b8578dbf020
RuiOkazaki <okazaki200226@gmail.com>
2021-10-16 18:12:58 +0900

    add: create Contact components

---
+++ src/components/organisms/Contact.tsx
@@ -0,0 +1,8 @@
+/* eslint-disable no-irregular-whitespace */
+import { VFC } from 'react';
+
+import SectionWrap from '../templates/SectionWrap';
+
+const Profile: VFC = () => <SectionWrap sectionTitle="Contact" />;
+
+export default Profile;
