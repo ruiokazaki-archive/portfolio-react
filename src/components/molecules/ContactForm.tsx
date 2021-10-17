7f29f51c8ad931855666ddf9b9dc919df96cca66
RuiOkazaki <okazaki200226@gmail.com>
2021-10-17 21:35:46 +0900

    add: create ContactForm components

---
+++ src/components/molecules/ContactForm.tsx
@@ -0,0 +1,10 @@
+import { VFC } from 'react';
+
+const SectionBackground: VFC = () => (
+  <form
+    action="https://click.ecc.ac.jp/ecc/rokazaki/sendmail.php"
+    method="post"
+  />
+);
+
+export default SectionBackground;
