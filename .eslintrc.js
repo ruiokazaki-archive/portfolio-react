7c42b220bda7d6887b79b546f4945f1c49e72691
RuiOkazaki <okazaki200226@gmail.com>
2021-10-19 20:53:37 +0900

    update: Label タグのルールを追加した

--- .eslintrc.js
+++ .eslintrc.js
@@ -102,6 +102,22 @@ module.exports = {
       },
     ],
     'react/react-in-jsx-scope': 'off',
+    'jsx-a11y/label-has-associated-control': [
+      'error',
+      {
+        required: {
+          some: ['nesting', 'id'],
+        },
+      },
+    ],
+    'jsx-a11y/label-has-for': [
+      'error',
+      {
+        required: {
+          some: ['nesting', 'id'],
+        },
+      },
+    ],
   },
   overrides: [
     {
