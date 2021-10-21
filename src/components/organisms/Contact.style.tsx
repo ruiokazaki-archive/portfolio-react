6f938a6cb732ff25146b7fced9094bb2e5f98aa4
RuiOkazaki <okazaki200226@gmail.com>
2021-10-21 17:15:56 +0900

    add: create Contact components

---
+++ src/components/organisms/Contact.style.tsx
@@ -0,0 +1,66 @@
+import styled from 'styled-components';
+import tw from 'tailwind-styled-components';
+
+const Col2WrapStyle = styled.div`
+  padding-top: 20px;
+  margin-bottom: 50px;
+`;
+export const Col2Wrap = tw(Col2WrapStyle)`flex justify-between`;
+
+const Col1Style = styled.div`
+  margin-bottom: 60px;
+`;
+export const Col1 = tw(Col1Style)`relative`;
+
+const FocusBorderStyle = styled.div`
+  background-color: #f858f1;
+  height: 2px;
+  transform-origin: center center;
+  transform: scaleX(0);
+  transition: all 0.3s;
+`;
+export const FocusBorder = tw(FocusBorderStyle)`absolute w-full bottom-0`;
+
+export const Input = styled.input`
+  display: block;
+  width: 100%;
+  min-height: 30px;
+  padding: 0 6px;
+  border-bottom: 2px solid #f2f2f2;
+  background-color: none;
+  color: #f2f2f2;
+  line-height: 21px;
+  letter-spacing: 1px;
+  &:-webkit-autofill {
+    box-shadow: 0 0 0px 40px #14121c inset !important;
+    -webkit-text-fill-color: #f2f2f2 !important;
+  }
+`;
+export const Textarea = styled.textarea`
+  display: block;
+  width: 100%;
+  min-height: 30px;
+  padding: 4px 6px;
+  border-bottom: 2px solid #f2f2f2;
+  background-color: none;
+  color: #f2f2f2;
+  line-height: 21px;
+  letter-spacing: 1px;
+  resize: none;
+`;
+const LabelStyle = styled.label`
+  left: 6px;
+  top: 4px;
+  letter-spacing: 1px;
+  transition: all 0.3s;
+`;
+export const Label = tw(LabelStyle)`absolute`;
+
+const Col2Style = styled.div`
+  width: 386px;
+
+  ${Input}:focus + ${FocusBorderStyle} {
+    transform: scaleX(1);
+  }
+`;
+export const Col2 = tw(Col2Style)`relative`;
--- src/components/organisms/Contact.tsx
+++ src/components/organisms/Contact.tsx
@@ -2,22 +2,38 @@ import { VFC } from 'react';
 
 import SectionWrap from '../templates/SectionWrap';
 import ContactForm from '../molecules/ContactForm';
-import ContactFormPartsWrap from '../templates/ContactFormPartsWrap';
+import {
+  Col2Wrap,
+  Col1,
+  Col2,
+  Input,
+  Textarea,
+  FocusBorder,
+  Label,
+} from './Contact.style';
 
 const Profile: VFC = () => (
   <SectionWrap sectionTitle="Contact">
     <ContactForm>
-      <div className="flex justify-between">
-        <ContactFormPartsWrap LabelText="Name">
-          <input type="text" />
-        </ContactFormPartsWrap>
-        <ContactFormPartsWrap LabelText="Email">
-          <input type="text" />
-        </ContactFormPartsWrap>
-      </div>
-      <ContactFormPartsWrap LabelText="Message">
-        <textarea />
-      </ContactFormPartsWrap>
+      <Col2Wrap>
+        <Col2>
+          <Label htmlFor="name">Name</Label>
+          <Input id="name" type="text" name="name" />
+          <FocusBorder />
+        </Col2>
+
+        <Col2>
+          <Label htmlFor="email">Email</Label>
+          <Input id="email" type="email" name="email" />
+          <FocusBorder />
+        </Col2>
+      </Col2Wrap>
+
+      <Col1>
+        <Label htmlFor="message">Message</Label>
+        <Textarea name="message" id="message" />
+        <FocusBorder />
+      </Col1>
     </ContactForm>
   </SectionWrap>
 );
