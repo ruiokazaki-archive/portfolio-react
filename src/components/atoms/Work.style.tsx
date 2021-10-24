8feb5f12934aea46b0b89793638288dbecf82f48
RuiOkazaki <okazaki200226@gmail.com>
2021-10-24 19:21:01 +0900

    update: 作品の style 等を作成

---
+++ src/components/atoms/Work.style.tsx
@@ -0,0 +1,16 @@
+import TiltEl from 'react-parallax-tilt';
+import styled from 'styled-components';
+
+export const Tilt = styled(TiltEl)`
+  width: 320px;
+  height: 180px;
+  transform-style: preserve-3d;
+  background: #f2f2f2;
+  border-radius: 40px;
+  box-shadow: rgba(50, 50, 93, 0.25) 0px 30px 60px -12px inset,
+    rgba(0, 0, 0, 0.3) 0px 18px 36px -18px inset;
+`;
+
+export const LogoWrap = styled.figure`
+  transform: translateZ(50px);
+`;
--- src/components/atoms/Work.tsx
+++ src/components/atoms/Work.tsx
@@ -1,12 +1,18 @@
 import { VFC } from 'react';
 import logo from '../../assets/svg/logo_nonst.svg';
+import { Tilt, LogoWrap } from './Work.style';
+import { WorkDataType } from '../../data/Works';
 
-const Work: VFC = () => (
-  <div className="work" data-tilt data-tilt-scale="1.05">
-    <figure>
-      <img src={logo} alt="作品" />
-    </figure>
-  </div>
+type Props = {
+  workData: WorkDataType;
+};
+
+const Work: VFC<Props> = ({ workData }) => (
+  <Tilt scale={1.05} perspective={1000}>
+    <LogoWrap>
+      <img src={logo} alt={`${workData.title}のロゴ`} />
+    </LogoWrap>
+  </Tilt>
 );
 
 export default Work;
