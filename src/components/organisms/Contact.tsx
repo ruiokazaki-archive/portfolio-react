de7e7141d3d5c22110d2a3fde3e60fe8741bbb05
RuiOkazaki <okazaki200226@gmail.com>
2021-10-24 11:32:42 +0900

    add: create EnancedContact Components

--- src/components/organisms/Contact.tsx
+++ src/components/organisms/Contact.tsx
@@ -1,4 +1,4 @@
-import { VFC, useRef, RefObject, ChangeEvent, FocusEvent } from 'react';
+import { VFC, RefObject, ChangeEvent, FocusEvent } from 'react';
 
 import SectionWrap from '../templates/SectionWrap';
 import ContactForm from '../molecules/ContactForm';
@@ -12,112 +12,83 @@ import {
   Label,
 } from './Contact.style';
 
-const Profile: VFC = () => {
-  const nameLabelRef = useRef<HTMLLabelElement>(null);
-  const emailLabelRef = useRef<HTMLLabelElement>(null);
-  const messageLabelRef = useRef<HTMLLabelElement>(null);
-  const messageRef = useRef<HTMLTextAreaElement>(null);
-
-  const labelRefClassChange = (
-    ref: RefObject<HTMLLabelElement>,
-    event: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLTextAreaElement>,
-  ) => {
-    if (ref.current === null) return;
-    if (event.target.value === '') {
-      ref.current.classList.remove('labelup');
-    } else {
-      ref.current.classList.add('labelup');
-    }
-  };
-  const labelElementClassChange = (
-    el: Element | undefined,
-    event: FocusEvent<HTMLInputElement | HTMLTextAreaElement>,
-  ) => {
-    if (el === undefined) return;
-    if (event.target.value === '') {
-      el.classList.remove('labelup');
-    } else {
-      el.classList.add('labelup');
-    }
-  };
-
-  const handleNameChange = (e: ChangeEvent<HTMLInputElement>) => {
-    labelRefClassChange(nameLabelRef, e);
-  };
-  const handleEmailChange = (e: ChangeEvent<HTMLInputElement>) => {
-    labelRefClassChange(emailLabelRef, e);
-  };
-  const handleMessageChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
-    labelRefClassChange(messageLabelRef, e);
-    if (messageRef.current !== null) {
-      messageRef.current.style.height = `${
-        messageRef.current.value.split('\n').length * 20 + 30
-      }px`;
-    }
-  };
-  const focusClassChange = (
+type Props = {
+  nameLabelRef: RefObject<HTMLLabelElement>;
+  emailLabelRef: RefObject<HTMLLabelElement>;
+  messageLabelRef: RefObject<HTMLLabelElement>;
+  messageRef: RefObject<HTMLTextAreaElement>;
+  handleNameChange: (e: ChangeEvent<HTMLInputElement>) => void;
+  handleEmailChange: (e: ChangeEvent<HTMLInputElement>) => void;
+  handleMessageChange: (e: ChangeEvent<HTMLTextAreaElement>) => void;
+  focusClassChange: (
     e: FocusEvent<HTMLInputElement | HTMLTextAreaElement>,
-  ) => {
-    e.target.parentElement?.children[0].classList.add('labelup');
-  };
-  const focusoutClassChange = (
+  ) => void;
+  focusoutClassChange: (
     e: FocusEvent<HTMLInputElement | HTMLTextAreaElement>,
-  ) => {
-    labelElementClassChange(e.target.parentElement?.children[0], e);
-  };
-
-  return (
-    <SectionWrap sectionTitle="Contact">
-      <ContactForm>
-        <Col2Wrap>
-          <Col2>
-            <Label ref={nameLabelRef} htmlFor="name">
-              Name
-            </Label>
-            <Input
-              id="name"
-              type="text"
-              name="name"
-              onChange={handleNameChange}
-              onFocus={focusClassChange}
-              onBlur={focusoutClassChange}
-            />
-            <FocusBorder />
-          </Col2>
-
-          <Col2>
-            <Label ref={emailLabelRef} htmlFor="email">
-              Email
-            </Label>
-            <Input
-              id="email"
-              type="email"
-              name="email"
-              onChange={handleEmailChange}
-              onFocus={focusClassChange}
-              onBlur={focusoutClassChange}
-            />
-            <FocusBorder />
-          </Col2>
-        </Col2Wrap>
-
-        <Col1>
-          <Label ref={messageLabelRef} htmlFor="message">
-            Message
-          </Label>
-          <Textarea
-            onChange={handleMessageChange}
-            onFocus={focusClassChange}
-            onBlur={focusoutClassChange}
-            name="message"
-            id="message"
-            ref={messageRef}
-          />
-          <FocusBorder />
-        </Col1>
-      </ContactForm>
-    </SectionWrap>
-  );
+  ) => void;
 };
 
-export default Profile;
+const Contact: VFC<Props> = ({
+  nameLabelRef,
+  emailLabelRef,
+  messageLabelRef,
+  messageRef,
+  handleNameChange,
+  handleEmailChange,
+  handleMessageChange,
+  focusClassChange,
+  focusoutClassChange,
+}) => (
+  <SectionWrap sectionTitle="Contact">
+    <ContactForm>
+      <Col2Wrap>
+        <Col2>
+          <Label ref={nameLabelRef} htmlFor="name">
+            Name
+          </Label>
+          <Input
+            id="name"
+            type="text"
+            name="name"
+            onChange={handleNameChange}
+            onFocus={focusClassChange}
+            onBlur={focusoutClassChange}
+          />
+          <FocusBorder />
+        </Col2>
+
+        <Col2>
+          <Label ref={emailLabelRef} htmlFor="email">
+            Email
+          </Label>
+          <Input
+            id="email"
+            type="email"
+            name="email"
+            onChange={handleEmailChange}
+            onFocus={focusClassChange}
+            onBlur={focusoutClassChange}
+          />
+          <FocusBorder />
+        </Col2>
+      </Col2Wrap>
+
+      <Col1>
+        <Label ref={messageLabelRef} htmlFor="message">
+          Message
+        </Label>
+        <Textarea
+          onChange={handleMessageChange}
+          onFocus={focusClassChange}
+          onBlur={focusoutClassChange}
+          name="message"
+          id="message"
+          ref={messageRef}
+        />
+        <FocusBorder />
+      </Col1>
+    </ContactForm>
+  </SectionWrap>
+);
+
+export default Contact;
