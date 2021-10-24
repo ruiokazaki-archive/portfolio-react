baea472cfa648476e6ffd3178ea2155e6c2aab23
RuiOkazaki <okazaki200226@gmail.com>
2021-10-24 16:07:45 +0900

    add: Works components を作成した

---
+++ src/components/organisms/Works.tsx
@@ -0,0 +1,10 @@
+import { VFC } from 'react';
+import SectionWrap from '../templates/SectionWrap';
+
+const About: VFC = () => (
+  <SectionWrap sectionTitle="Works">
+    <p>test</p>
+  </SectionWrap>
+);
+
+export default About;
--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,12 +1,15 @@
 import { VFC } from 'react';
+
 import About from '../organisms/About';
 import Profile from '../../containers/organisms/Profile';
+import Works from '../organisms/Works';
 import Contact from '../../containers/organisms/Contact';
 
 const Home: VFC = () => (
   <>
     <About />
     <Profile />
+    <Works />
     <Contact />
   </>
 );
