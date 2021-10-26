cc1980d284ce85322152e56405a380ef3b7add39
RuiOkazaki <okazaki200226@gmail.com>
2021-10-26 19:10:17 +0900

    add: create HeaderLogo components

---
+++ src/components/atoms/HeaderLogo.style.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+import tw from 'tailwind-styled-components';
+
+const LogoStyle = styled.h1`
+  font-size: 22px;
+`;
+export const Logo = tw(LogoStyle)`text-white1`;
+
+export const Pink = tw.span`text-pink1`;
---
+++ src/components/atoms/HeaderLogo.tsx
@@ -0,0 +1,9 @@
+import { VFC } from 'react';
+import { Logo, Pink } from './HeaderLogo.style';
+
+const HeaderLogo: VFC = () => (
+  <Logo>
+    <Pink>&lt;</Pink> ruru&apos;s portfolio <Pink>/&gt;</Pink>
+  </Logo>
+);
+export default HeaderLogo;
