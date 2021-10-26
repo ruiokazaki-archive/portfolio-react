b3cd7214722aceaa4a1613d1ff4f18153c2b026d
RuiOkazaki <okazaki200226@gmail.com>
2021-10-26 20:34:26 +0900

    update: 色が自然に変わるように変更

--- src/components/molecules/HeaderNav.style.tsx
+++ src/components/molecules/HeaderNav.style.tsx
@@ -8,6 +8,7 @@ const UlStyle = styled.ul`
 export const Ul = tw(UlStyle)`text-gray1 flex justify-between items-center `;
 
 export const Li = styled.li`
+  transition: all 0.3s;
   cursor: pointer;
   &:hover {
     color: #f2f2f2;
