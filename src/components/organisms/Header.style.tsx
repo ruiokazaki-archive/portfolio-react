db54300ad0ca184cf0cd21a7f092fb7159d8ab41
RuiOkazaki <okazaki200226@gmail.com>
2021-10-26 19:42:59 +0900

    update: header に高さをと余白を設けた

--- src/components/organisms/Header.style.tsx
+++ src/components/organisms/Header.style.tsx
@@ -3,8 +3,10 @@ import tw from 'tailwind-styled-components';
 
 const HeaderWrapStyle = styled.header`
   z-index: 100000;
+  padding: 0 30px;
+  height: 64px;
 `;
 const HeaderWrap = tw(
   HeaderWrapStyle,
-)`fixed top-0 left-0 flex justify-between items-center w-full`;
+)`fixed top-0 left-0 flex justify-between items-center w-full font-bold`;
 export default HeaderWrap;
