e419162346d66785459528fd9f3c39dd466fb9ad
RuiOkazaki <okazaki200226@gmail.com>
2021-11-01 19:50:53 +0900

    update: id Start がない時は実行しないようにする

--- public/script/vanta.js
+++ public/script/vanta.js
@@ -1,20 +1,22 @@
 window.addEventListener('load', () => {
-  VANTA.BIRDS({
-    el: '#Start',
-    mouseControls: true,
-    touchControls: true,
-    gyroControls: true,
-    scale: 1,
-    scaleMobile: 1,
-    backgroundColor: '#110e17',
-    color1: '#4f0f4c',
-    color2: '#4f0f4c',
-    colorMode: 'variance',
-    birdSize: 1,
-    wingSpan: 20,
-    speedLimit: 8,
-    separation: 29,
-    alignment: 33,
-    cohesion: 50,
-  });
+  if (document.getElementById('Start')) {
+    VANTA.BIRDS({
+      el: '#Start',
+      mouseControls: true,
+      touchControls: true,
+      gyroControls: true,
+      scale: 1,
+      scaleMobile: 1,
+      backgroundColor: '#110e17',
+      color1: '#4f0f4c',
+      color2: '#4f0f4c',
+      colorMode: 'variance',
+      birdSize: 1,
+      wingSpan: 20,
+      speedLimit: 8,
+      separation: 29,
+      alignment: 33,
+      cohesion: 50,
+    });
+  }
 });
