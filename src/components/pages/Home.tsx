37f6bd6bb37ee394afbecdbd9f5c8d16d6f6da29
RuiOkazaki <okazaki200226@gmail.com>
2022-01-24 00:50:05 +0900

    add: create about page

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -2,7 +2,7 @@ import { VFC } from 'react';
 
 import Header from 'components/organisms/Header';
 import FirstView from 'containers/organisms/FirstView';
-import About from 'components/organisms/About';
+import About from 'containers/organisms/About';
 import Works from 'components/organisms/Works';
 import Footer from 'components/organisms/Footer';
 import Profile from 'containers/organisms/Profile';
