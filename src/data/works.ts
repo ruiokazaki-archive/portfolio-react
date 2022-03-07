f2fb694c1af28e4ae4cc92b559542c1c922ae574
RuiOkazaki <okazaki200226@gmail.com>
2022-03-07 17:14:57 +0900

    fix: 作品詳細ページが正しく表示されないバグを修正

--- src/data/works.ts
+++ src/data/works.ts
@@ -1,23 +1,24 @@
 import InnerPeaceCafeImg from 'assets/images/InnerPeaceCafe_screen.jpg';
 import InnerPeaceCafe from 'assets/svg/InnerPeaceCafe_logo.svg';
-// import NOMINOImg from 'assets/images/NOMINO_screen.jpg';
-// import NOMINO from 'assets/svg/NOMINO_logo.svg';
-// import tappinessImg from 'assets/images/tappiness_screen.jpg';
-// import tappiness from 'assets/svg/tappiness_logo.svg';
 import SparkImg from 'assets/images/spark_screen.jpg';
-import Spark from 'assets/svg/spark_logo.svg';
+import Spark from 'assets/svg/Spark_logo.svg';
 import portfolioImg from 'assets/images/rurus-portfolio_screen.jpg';
 import portfolio from 'assets/svg/rurus-portfolio_logo.svg';
 import ECCImg from 'assets/images/ECC_screen.jpg';
 import ECC from 'assets/svg/ECC_logo.svg';
+import NONSTImg from 'assets/images/nonst_screen.jpg';
+import NONST from 'assets/svg/NONST_logo.svg';
+import umiToKokoroToBokuImg from 'assets/images/umiToKokoroToBoku_screen.jpg';
+import umiToKokoroToBoku from 'assets/svg/umiToKokoroToBoku_logo.svg';
+
+// import NOMINOImg from 'assets/images/NOMINO_screen.jpg';
+// import NOMINO from 'assets/svg/NOMINO_logo.svg';
+// import tappinessImg from 'assets/images/tappiness_screen.jpg';
+// import tappiness from 'assets/svg/tappiness_logo.svg';
 // import CinemaTangerineImg from 'assets/images/CinemaTangerine_screen.jpg';
 // import CinemaTangerine from 'assets/svg/CinemaTangerine_logo.svg';
 // import FriboImg from 'assets/images/Fribo_screen.jpg';
 // import Fribo from 'assets/svg/Fribo_logo.svg';
-import NONSTImg from 'assets/images/nonst_screen.jpg';
-import NONST from 'assets/svg/nonst_logo.svg';
-import umiToKokoroToBokuImg from 'assets/images/umiToKokoroToBoku_screen.jpg';
-import umiToKokoroToBoku from 'assets/svg/umiToKokoroToBoku_logo.svg';
 
 export type linkType = {
   name: string;
@@ -69,65 +70,8 @@ export const worksData: WorkDataType[] = [
     img: InnerPeaceCafeImg,
     logo: InnerPeaceCafe,
   },
-  // {
-  //   id: 2,
-  //   title: 'NOMINO',
-  //   description:
-  //     'nominoは売り手の手間を最小限にした。面倒臭いがないフリマアプリです。買い手がすぐ見つかり従来のフリマアプリとは違いスピーディーな取引が可能です。',
-  //   impressions:
-  //     'vuejsの勉強のために作成した初めてのスマートフォン向けwebアプリケーションです。',
-  //   teamProduction: 0,
-  //   position: 'Planning, Design, FrontEnd',
-  //   usetime: 16,
-  //   season: '1年後期',
-  //   programmingLanguages:
-  //     'Photoshop, Illustrator, Figma, HTML5, SCSS, JavaScript, Vuejs',
-  //   toolsUsed: 'Photoshop, Illustrator, Figma, HTML5, SCSS, JavaScript, Vuejs',
-  //   githubLink: [
-  //     {
-  //       name: 'APP',
-  //       url: 'https://github.com/OkazakiRui/nomino',
-  //     },
-  //   ],
-  //   productionLink: [
-  //     {
-  //       name: 'APP',
-  //       url: 'https://click.ecc.ac.jp/ecc/rokazaki/works/nomino/',
-  //     },
-  //   ],
-  //   img: NOMINOImg,
-  //   logo: NOMINO,
-  // },
-  // {
-  //   id: 3,
-  //   title: 'たっぴねす',
-  //   description:
-  //     'たっぴねすはやる気がでない人がやる気を出すためのwebアプリです。<br />目標達成ができない人に達成しないと欲しいものが手に入らないようにすることで普段頑張れない人も頑張ることが出来ます。',
-  //   impressions:
-  //     '初めてのチーム制作でした。チームで動くことの難しさを身をもって体験しました。',
-  //   teamProduction: 1,
-  //   position: 'Planning, FrontEnd',
-  //   usetime: 30,
-  //   season: '1年後期',
-  //   programmingLanguages: 'HTML5, CSS3, Sass, JavaScript, Vuejs',
-  //   toolsUsed: 'HTML5, CSS3, Sass, JavaScript, Vuejs',
-  //   githubLink: [
-  //     {
-  //       name: 'APP',
-  //       url: 'https://github.com/OkazakiRui/tappiness',
-  //     },
-  //   ],
-  //   productionLink: [
-  //     {
-  //       name: 'APP',
-  //       url: 'https://click.ecc.ac.jp/ecc/rokazaki/works/tappiness/',
-  //     },
-  //   ],
-  //   img: tappinessImg,
-  //   logo: tappiness,
-  // },
   {
-    id: 4,
+    id: 2,
     title: 'Spark',
     description:
       'Sparkはビジネスではなく、オンライン呑み会などカジュアルにビデオ通話をするための新しいツールです。無言が続くと画面にエフェクトが起こったり、豊富なスタンプが特徴的です。',
@@ -155,7 +99,7 @@ export const worksData: WorkDataType[] = [
     logo: Spark,
   },
   {
-    id: 5,
+    id: 3,
     title: 'portfolio Vuejs',
     description:
       '自分の将来したいことや、今まで作った作品をまとめたwebサイトを作成しました。',
@@ -183,7 +127,7 @@ export const worksData: WorkDataType[] = [
     logo: portfolio,
   },
   {
-    id: 6,
+    id: 4,
     title: 'ECC学生アプリ',
     description:
       '現在apple storeにあるECC学生アプリは学生の間で使いにくいと評判があり、学校側も学生が通知欄を見ないので困っています。<br />そこで、ECC学生アプリを本来の機能とプラスして使いやすい機能を搭載して実装することで学生が使い続けたくなるアプリを作成したいと考えました。',
@@ -210,73 +154,8 @@ export const worksData: WorkDataType[] = [
     img: ECCImg,
     logo: ECC,
   },
-  // {
-  //   id: 7,
-  //   title: 'CinemaTangerine',
-  //   description:
-  //     '若年者ものづくり競技大会にて銀賞をいただきました。<br />お題とapiが与えられ時間内に解決方法を考え、解決に沿ったデザイン、コーディング、プレゼン資料作成を計5時間で行う旨の大会でした。',
-  //   impressions:
-  //     '5時間という短い時間内に企画、デザイン、レスポンシブ対応、api通信、フィルター機能、プレゼンテーションの資料を作成することができ、満足のいく結果に終わったと思う！',
-  //   teamProduction: 1,
-  //   position: 'Planning, Design, FrontEnd',
-  //   usetime: 5,
-  //   season: '8月5日',
-  //   programmingLanguages: 'HTML5, CSS3, JavaScript',
-  //   toolsUsed: 'なし',
-  //   githubLink: [
-  //     {
-  //       name: 'LP',
-  //       url: 'https://github.com/OkazakiRui/jakunensha-monodukuri',
-  //     },
-  //   ],
-  //   productionLink: [
-  //     {
-  //       name: 'LP',
-  //       url:
-  //         'https://click.ecc.ac.jp/ecc/rokazaki/works/jakunensha-monodukuri/',
-  //     },
-  //   ],
-  //   img: CinemaTangerineImg,
-  //   logo: CinemaTangerine,
-  // },
-  // {
-  //   id: 8,
-  //   title: 'Fribo',
-  //   description:
-  //     '女性または一人暮らしの方の悩みである、夜道が怖い。を解決するために作られたwebアプリです。',
-  //   impressions:
-  //     'firebaseを初めて使い、PWAにも初めてしたので、初めてづくしでとても勉強になった',
-  //   teamProduction: 1,
-  //   position: 'Planning, FrontEnd, BackEnd',
-  //   usetime: 40,
-  //   season: '2年前期',
-  //   programmingLanguages: 'Vuejs, Firebase',
-  //   toolsUsed: 'Vuejs, Firebase',
-  //   githubLink: [
-  //     {
-  //       name: 'LP',
-  //       url: 'https://github.com/OkazakiRui/Fribo-LP',
-  //     },
-  //     {
-  //       name: 'APP',
-  //       url: 'https://github.com/OkazakiRui/Fribo-App',
-  //     },
-  //   ],
-  //   productionLink: [
-  //     {
-  //       name: 'LP',
-  //       url: 'https://click.ecc.ac.jp/ecc/rokazaki/works/Fribo-LP/',
-  //     },
-  //     {
-  //       name: 'APP',
-  //       url: 'https://click.ecc.ac.jp/ecc/rokazaki/works/Fribo-App/',
-  //     },
-  //   ],
-  //   img: FriboImg,
-  //   logo: Fribo,
-  // },
   {
-    id: 9,
+    id: 5,
     title: 'NONST',
     description:
       '社会人になってストレスが溜まり、中高の友達とも疎遠になっていて愚痴る相手が居ない。を解決するマッチングアプリです。',
@@ -312,7 +191,7 @@ export const worksData: WorkDataType[] = [
     logo: NONST,
   },
   {
-    id: 10,
+    id: 6,
     title: 'portfolio React',
     description:
       '過去に作ったポートフォリオサイトを一部デザイン変更し、リニューアルしました。',
@@ -339,7 +218,7 @@ export const worksData: WorkDataType[] = [
     logo: portfolio,
   },
   {
-    id: 11,
+    id: 7,
     title: 'うみとこころとぼく',
     description:
       '普段からSNSを運用していると、ふとした発言が誰かを傷つけてしまったり、少し前の自分の投稿すらも過度に気にしてしまい、なんてことない呟きすらも億劫になってしまいます。<br />SNSで疲れた現代人が、ゆったりまったりとした世界観でリラックスできるSNSがあったらいいなと思い、このサービスを作成しました。',
@@ -375,3 +254,126 @@ export const worksData: WorkDataType[] = [
     logo: umiToKokoroToBoku,
   },
 ];
+
+// {
+//   id: 2,
+//   title: 'NOMINO',
+//   description:
+//     'nominoは売り手の手間を最小限にした。面倒臭いがないフリマアプリです。買い手がすぐ見つかり従来のフリマアプリとは違いスピーディーな取引が可能です。',
+//   impressions:
+//     'vuejsの勉強のために作成した初めてのスマートフォン向けwebアプリケーションです。',
+//   teamProduction: 0,
+//   position: 'Planning, Design, FrontEnd',
+//   usetime: 16,
+//   season: '1年後期',
+//   programmingLanguages:
+//     'Photoshop, Illustrator, Figma, HTML5, SCSS, JavaScript, Vuejs',
+//   toolsUsed: 'Photoshop, Illustrator, Figma, HTML5, SCSS, JavaScript, Vuejs',
+//   githubLink: [
+//     {
+//       name: 'APP',
+//       url: 'https://github.com/OkazakiRui/nomino',
+//     },
+//   ],
+//   productionLink: [
+//     {
+//       name: 'APP',
+//       url: 'https://click.ecc.ac.jp/ecc/rokazaki/works/nomino/',
+//     },
+//   ],
+//   img: NOMINOImg,
+//   logo: NOMINO,
+// },
+// {
+//   id: 3,
+//   title: 'たっぴねす',
+//   description:
+//     'たっぴねすはやる気がでない人がやる気を出すためのwebアプリです。<br />目標達成ができない人に達成しないと欲しいものが手に入らないようにすることで普段頑張れない人も頑張ることが出来ます。',
+//   impressions:
+//     '初めてのチーム制作でした。チームで動くことの難しさを身をもって体験しました。',
+//   teamProduction: 1,
+//   position: 'Planning, FrontEnd',
+//   usetime: 30,
+//   season: '1年後期',
+//   programmingLanguages: 'HTML5, CSS3, Sass, JavaScript, Vuejs',
+//   toolsUsed: 'HTML5, CSS3, Sass, JavaScript, Vuejs',
+//   githubLink: [
+//     {
+//       name: 'APP',
+//       url: 'https://github.com/OkazakiRui/tappiness',
+//     },
+//   ],
+//   productionLink: [
+//     {
+//       name: 'APP',
+//       url: 'https://click.ecc.ac.jp/ecc/rokazaki/works/tappiness/',
+//     },
+//   ],
+//   img: tappinessImg,
+//   logo: tappiness,
+// },
+// {
+//   id: 7,
+//   title: 'CinemaTangerine',
+//   description:
+//     '若年者ものづくり競技大会にて銀賞をいただきました。<br />お題とapiが与えられ時間内に解決方法を考え、解決に沿ったデザイン、コーディング、プレゼン資料作成を計5時間で行う旨の大会でした。',
+//   impressions:
+//     '5時間という短い時間内に企画、デザイン、レスポンシブ対応、api通信、フィルター機能、プレゼンテーションの資料を作成することができ、満足のいく結果に終わったと思う！',
+//   teamProduction: 1,
+//   position: 'Planning, Design, FrontEnd',
+//   usetime: 5,
+//   season: '8月5日',
+//   programmingLanguages: 'HTML5, CSS3, JavaScript',
+//   toolsUsed: 'なし',
+//   githubLink: [
+//     {
+//       name: 'LP',
+//       url: 'https://github.com/OkazakiRui/jakunensha-monodukuri',
+//     },
+//   ],
+//   productionLink: [
+//     {
+//       name: 'LP',
+//       url:
+//         'https://click.ecc.ac.jp/ecc/rokazaki/works/jakunensha-monodukuri/',
+//     },
+//   ],
+//   img: CinemaTangerineImg,
+//   logo: CinemaTangerine,
+// },
+// {
+//   id: 8,
+//   title: 'Fribo',
+//   description:
+//     '女性または一人暮らしの方の悩みである、夜道が怖い。を解決するために作られたwebアプリです。',
+//   impressions:
+//     'firebaseを初めて使い、PWAにも初めてしたので、初めてづくしでとても勉強になった',
+//   teamProduction: 1,
+//   position: 'Planning, FrontEnd, BackEnd',
+//   usetime: 40,
+//   season: '2年前期',
+//   programmingLanguages: 'Vuejs, Firebase',
+//   toolsUsed: 'Vuejs, Firebase',
+//   githubLink: [
+//     {
+//       name: 'LP',
+//       url: 'https://github.com/OkazakiRui/Fribo-LP',
+//     },
+//     {
+//       name: 'APP',
+//       url: 'https://github.com/OkazakiRui/Fribo-App',
+//     },
+//   ],
+//   productionLink: [
+//     {
+//       name: 'LP',
+//       url: 'https://click.ecc.ac.jp/ecc/rokazaki/works/Fribo-LP/',
+//     },
+//     {
+//       name: 'APP',
+//       url: 'https://click.ecc.ac.jp/ecc/rokazaki/works/Fribo-App/',
+//     },
+//   ],
+//   img: FriboImg,
+//   logo: Fribo,
+// },
